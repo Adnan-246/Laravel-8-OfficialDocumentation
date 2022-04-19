cde3132fdf9887bbfee51c84040664f0bfe69ebc
Habib <diu.swe.habib@gmail.com>
2022-04-20 03:49:16 +0600

    CSRF Token

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,13 +2,26 @@
 <a href="{{ url('/') }}">Back</a><br><br>
 
 
-<div class="container">
-  <form action="{{ route('student.store') }}" method="POST">
-    @csrf
-    
-    <input type="text" name="name" placeholder="Enter your name"><br><br>
-    <input type="email" name="email" placeholder="Enter your email"><br><br>
-    <button type="submit">Submit</button>
-
-</form>
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Contact</title>
+</head>
+<body>
+  <h1>Store Your Contact Person</h1><br><br>
+  <div class="container">
+    <form action="{{ route('student.store') }}" method="POST">
+      @csrf
+  
+      <input type="text" name="name" placeholder="Enter your name"><br><br>
+      <input type="email" name="email" placeholder="Enter your email"><br><br>
+      <input type="text" name="phone" placeholder="Enter your phone"><br><br>
+      <button type="submit" class="btn btn-primary">Submit</button>
+  
+  </form>
+  </div>
+</body>
+</html>
